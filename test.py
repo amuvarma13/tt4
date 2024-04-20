b27ad6738abda3b55701181e4538dd2b3aff0e2d
Amartya Varma <amartya.varma@gmail.com>
2024-04-20 16:13:11 +0100

    f

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from scipy.io.wavfile import write
 import ljinference
 import torch
-text = 'Hello world!'
+text = 'Hello this the lj inference test.'
 noise = torch.randn(1,1,256).to('cuda' if torch.cuda.is_available() else 'cpu')
 wav = ljinference.inference(text, noise, diffusion_steps=7, embedding_scale=1)
-write('result.wav', 24000, wav)
+write('resultlj.wav', 24000, wav)
