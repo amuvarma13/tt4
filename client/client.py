3267576bbf1ef073d7a82bd2f7030e93ed23ca0b
Amartya Varma <amartya.varma@gmail.com>
2024-04-23 15:41:31 +0100

    f

--- client/client.py
+++ client/client.py
@@ -1,47 +1,55 @@
 import requests
 import time
+import concurrent.futures
 
-api_url = 'http://34.91.239.71:8080/api/v1/static'
-ping_url = 'http://34.91.239.71:8080/ping'
+api_url = 'http://34.141.243.146:8080/api/v1/static'
+#api_url = 'http://127.0.0.1:8080/api/v1/static'
+ping_url = 'http://34.141.243.146:8080/ping'
 
-text = "How are you doing today?"
+text = '''That's "great news"! Congratulations man!'''
 
 def send_text_to_speech_request():
     # Parameters to send to the API
     data = {
         'text': text,
-        'voice': "m-us-3", 
+        'voice': "slow-emphatic", 
         'steps': 7, 
-        'alpha': 0.1,
-        'beta': 0.1
+        'alpha': 0.2,
+        'beta': 0.7,
+        'speed': 0.8, 
+        "embedding_scale":1
     }
     
     # Send POST request to the API
-    startTime = time.time()
     response = requests.post(api_url, data=data)
+   
+    print(response)
+
+
+# Modified function to include timing
+def timed_send_text_to_speech_request(start_time):
+    send_text_to_speech_request()
+    print(f"Time taken: {time.time() - start_time} seconds")
+    return time.time() - start_time
+
+def spam_requests(num_requests):
+    startTime = time.time()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=num_requests) as executor:
+        # Submitting the timed version of the function and collecting future objects
+        futures = [executor.submit(timed_send_text_to_speech_request, startTime) for _ in range(num_requests)]
+        # Retrieving results as they complete
+        latencies = [future.result() for future in concurrent.futures.as_completed(futures)]
+    
+    # Calculating average latency
+    
     endTime = time.time()
 
-    # Measure ping
-    ping_start = time.time()
-    response_ping = requests.post(api_url, data=data)
-    ping_end = time.time()
-    ping_dur = ping_end - ping_start
-    print(f"Time taken: {endTime - startTime - ping_dur} seconds")
+    print(f"Total time taken: {endTime - startTime} seconds")
 
+    return latencies
 
-    
-    # Check if the request was successful
-    if response.status_code == 200:
-        # Save the audio response
-        with open('output.wav', 'wb') as f:
-            f.write(response.content)
-        print("Audio file has been saved as 'output.wav'")
-    else:
-        # Print the error response
-        print("Error:", response.json()['error'])
+# Number of concurrent requests
+num_requests = 20
 
-# URL of the API
-
-
-# Example usage of the function
-send_text_to_speech_request()
+# Calling the function to spam requests and measure latency
+spam_requests(num_requests)
